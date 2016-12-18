3323d11c1c16dacc06770b3e92cb8a7063d3158a
Boaz Raz <raz.boaz@gmail.com>
2016-12-18 00:52:38 -0500

    final

--- 3-test.c
+++ 3-test.c
@@ -1,3 +1,4 @@
+// Boaz Raz
 
 #include <pcap.h>
 #include <stdio.h>
@@ -69,17 +70,10 @@ struct sniff_tcp {
 };
 
 void
-got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet);
+assemble_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet);
 
 void
 total_number_packets(u_char *args, const struct pcap_pkthdr *header, const u_char *packet);
-
-// method to count the number of packets
-void
-print_payload(const u_char *payload, int len);
-
-void
-print_hex_ascii_line(const u_char *payload, int len, int offset);
 
 /*
 * calculate the number of packets - task 1
@@ -98,15 +92,11 @@ total_number_packets(u_char *args, const struct pcap_pkthdr *header, const u_cha
 	const struct sniff_ethernet *ethernet;  /* The ethernet header [1] */
 	const struct sniff_ip *ip;              /* The IP header */
 	const struct sniff_tcp *tcp;            /* The TCP header */
-	const char *payload;                    /* Packet payload */
-
 	int size_ip;
 	int size_tcp;
-	int size_payload;
-	
-	// printf("\nPacket number %d:\n", count);
+
 	count++;
-	
+
 	/* define ethernet header */
 	ethernet = (struct sniff_ethernet*)(packet);
 	
@@ -146,10 +136,8 @@ total_number_packets(u_char *args, const struct pcap_pkthdr *header, const u_cha
 
 
 void
-got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
-{
-
-	
+assemble_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
+{	
 	
 	/* declare pointers to packet headers */
 	const struct sniff_ethernet *ethernet;  /* The ethernet header [1] */
@@ -175,103 +163,55 @@ got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
 		return;
 	}
 
-	/* determine protocol */	
-	switch(ip->ip_p) {
-		case IPPROTO_TCP:
-			// printf("   Protocol: TCP %d:\n", count);
-			break;
-		case IPPROTO_UDP:
-			// printf("   Protocol: UDP %d:\n", count);
-			return;
-		case IPPROTO_ICMP:
-			// printf("   Protocol: ICMP\n");
-			return;
-		case IPPROTO_IP:
-			// printf("   Protocol: IP\n");
-			return;
-		default:
-			// printf("   Protocol: unknown\n");
-			return;
-	}
-	
-
 	/* define/compute tcp header offset */
 	tcp = (struct sniff_tcp*)(packet + SIZE_ETHERNET + size_ip);
 	size_tcp = TH_OFF(tcp)*4;
 	if (size_tcp < 20) {
-		printf("   * Invalid TCP header length: %u bytes\n", size_tcp);
+		// printf("   * Invalid TCP header length: %u bytes\n", size_tcp);
 		return;
 	}
 	
-	// printf("%s %d %s %d \n", inet_ntoa(ip->ip_src),ntohs(tcp->th_sport),
-			// inet_ntoa(ip->ip_dst), ntohs(tcp->th_dport));
-	
+	printf("size_ip %d\n", size_ip);
 	printf("%s", inet_ntoa(ip->ip_src));
 	printf(" %d", ntohs(tcp->th_sport));
 	printf(" %s", inet_ntoa(ip->ip_dst));
 	printf(" %d\n", ntohs(tcp->th_dport));
 	
-
-	/* define/compute tcp payload (segment) offset */
-	payload = (u_char *)(packet + SIZE_ETHERNET + size_ip + size_tcp);
-	
-	/* compute tcp payload (segment) size */
-	size_payload = ntohs(ip->ip_len) - (size_ip + size_tcp);
-	
-	/*
-	 * Print payload data; it might be binary, so don't just
-	 * treat it as a string.
-	 */
-	if (size_payload > 0) {
-		printf("   Payload (%d bytes):\n", size_payload);
-		print_payload(payload, size_payload);
-	}
-
 return;
 }
 
 int main(int argc, char **argv)
 {
 
+	// char input
+	char *file_name = argv[2];
+	char *task_number = argv[1];
+
 	char *dev = "en0";			/* capture device name */
 	char errbuf[PCAP_ERRBUF_SIZE];		/* error buffer */
 	// pcap_t *handle;				/* packet capture handle */
-	pcap_t *handle = pcap_open_offline("task1.test1.pcap", errbuf);
+	pcap_t *handle = pcap_open_offline(file_name, errbuf);
 
 	char filter_exp[] = "ip";		/* filter expression [3] */
 	struct bpf_program fp;			/* compiled filter program (expression) */
 	bpf_u_int32 mask;			/* subnet mask */
 	bpf_u_int32 net;			/* ip */
-	// int num_packets = 100;			/* number of packets to capture */
 
-
-	/* compile the filter expression */
-	if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
-		fprintf(stderr, "Couldn't parse filter %s: %s\n",
-		    filter_exp, pcap_geterr(handle));
-		exit(EXIT_FAILURE);
-	}
-
-	/* apply the compiled filter */
-	if (pcap_setfilter(handle, &fp) == -1) {
-		fprintf(stderr, "Couldn't install filter %s: %s\n",
-		    filter_exp, pcap_geterr(handle));
-		exit(EXIT_FAILURE);
-	}
+	if (*task_number == '1'){
+		pcap_loop(handle,0 ,total_number_packets, NULL);		
+	 	printf("%d %d %d %d\n",count,count_ip, count_tcp, count_udp);
+	} 
+	else if (*task_number == '2'){
+		pcap_loop(handle,0 ,assemble_packet, NULL);	
+	} 
 
 	/* now we can set our callback function */
-	pcap_loop(handle,0 ,got_packet, NULL);
-
-	// pcap_loop(handle,0 ,total_number_packets, NULL);
-
-	// printf("%d %d %d %d\n",count,count_ip, count_tcp, count_udp);
-
+	pcap_loop(handle,0 ,assemble_packet, NULL);
 
 	/* cleanup */
+	// printf("argv:  %s\n", argv[1]);
 	pcap_freecode(&fp);
 	pcap_close(handle);
-
-	printf("\nCapture complete.\n");
 
 return 0;
 }
